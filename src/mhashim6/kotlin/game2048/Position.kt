dd4c57aa69719bf89fea639097e6747a1856ad8a
Muhammad Hashim <abohashem.com@gmail.com>
2018-04-20 00:11:07 +0200

    simplified Position class.

--- src/mhashim6/kotlin/game2048/Position.kt
+++ src/mhashim6/kotlin/game2048/Position.kt
@@ -1,39 +1,8 @@
 package mhashim6.kotlin.game2048
 
-class Position {
-
-    var row: Int = 0
-        private set
-    var col: Int = 0
-        private set
+data class Position(val row: Int = 0, val col: Int = 0) {
 
     constructor(position: Position) : this(position.row, position.col)
-
-    constructor(row: Int, col: Int) {
-        testArgs(row, col)
-        set(row, col)
-    }
-
-    private fun testArgs(row: Int, col: Int) {
-        if (row < 0)
-            throw IllegalArgumentException(String.format("negative rows are not allowed: %d", row))
-
-        if (col < 0)
-            throw IllegalArgumentException(String.format("negative columns are not allowed: %d", col))
-    }
-
-    private fun set(row: Int, col: Int) {
-        this.row = row
-        this.col = col
-    }
-
-    override fun equals(other: Any?): Boolean {
-        return other is Position && row == other.row && col == other.col
-    }
-
-    override fun hashCode(): Int {
-        return 31 * row + col
-    }
 
     companion object {
         val empty = Position(0, 0)
